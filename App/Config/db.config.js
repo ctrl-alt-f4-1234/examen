4da3b15c7a1759541eeb63b294d513c3cc4ed896
ctrl-alt-f4-1234 <chinchilla_edy@colgalileoantigua.edu.gt>
2026-06-05 11:27:03 -0600

    Correcion 8 examen

--- App/Config/db.config.js
+++ App/Config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
 
   HOST: "ep-falling-star-aqkkose1-pooler.c-8.us-east-1.aws.neon.tech",
   USER: "neondb_owner",
-  PASSWORD: "npg_Ey1BSDP5vFwb@ep",
+  PASSWORD: "npg_OUaxwcr2mCt7",
   DB: "neondb",
   dialect: "postgresql",
   pool:
